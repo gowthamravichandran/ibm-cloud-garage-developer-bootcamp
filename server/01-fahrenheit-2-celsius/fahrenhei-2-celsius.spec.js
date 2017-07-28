ac98dc92f3e1dd17a09eff2f694c4f47c17933f2
Gowtham Ravichandran <gowtham.ravichandran@coda.global>
2017-07-28 14:11:13 -0500

    make 2nd test pass and convert fahrenheit to celsius

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,13 +1,21 @@
-describe('the fahrenheit2celsius canary spec', () => {
+describe.only('the fahrenheit2celsius canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
+  function fahrenheit2celsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+
+
   describe('fahrenheit2celsius should', () => {
     it('give 0°C for 32°F', () => {
+      fahrenheit2celsius(32).should.equal(0);
+    });
+    it('give 10°C for 50°F', () => {
+      fahrenheit2celsius(50).should.equal(10);
     });
 
-    it('give 10°C for 50°F', () => {
-    });
+
   });
 });
