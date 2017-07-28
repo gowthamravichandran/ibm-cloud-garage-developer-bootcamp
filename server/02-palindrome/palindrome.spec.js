681de69571fc4cecc30709a8a0c55318f7d0d07f
Gowtham Ravichandran <gowtham.ravichandran@coda.global>
2017-07-28 15:19:21 -0500

    make 2 tests pass

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,2 +1,29 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
+  it('shows the infrastructure works', () => {
+      true.should.be.true();
+    });
+
+  function isPalindrome(text) {
+    return text.split('').reverse().join('') === text;
+  }
+  describe('a palindrome', () => {
+    it('is a RACECAR', () => {
+      isPalindrome('RACECAR').should.equal(true);
+    });
+    it('is a RADAR', () => {
+      isPalindrome('RADAR').should.equal(true);
+    });
+    it('is not RACECARS', () => {
+      isPalindrome('RACECARS').should.equal(false);
+    });
+    it('is not RACE CAR', () => {
+      isPalindrome('RACE CAR').should.equal(false);
+    });
+    it('', () => {
+      isPalindrome('RACE CAR').should.equal(false);
+    });
+    it('example1', () => {
+      isPalindrome('RACE CAR').should.equal(false);
+    });
+  });
 });
